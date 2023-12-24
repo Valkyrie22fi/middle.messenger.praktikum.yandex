4e8840205868e6b6ee8fe995d45fecfb3f82727f
valkyrie22fi <admin@afigol.ru>
2023-12-25 01:55:17 +0300

    fix

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,10 @@
-import { defineConfig } from 'vite'
+import { defineConfig } from "vite";
 
 export default defineConfig({
   build: {
-    outDir: 'dist',
+    outDir: "dist",
   },
   server: {
     port: 3000,
   },
-})
+});
