70bd51995255575e000156633b920abd1468d609
valkyrie22fi <admin@afigol.ru>
2024-01-08 13:50:23 +0300

    ESLint vs Pretter

--- src/main.ts
+++ src/main.ts
@@ -1,37 +1,36 @@
-import Handlebars from 'handlebars';
+import Handlebars from "handlebars";
 
-import * as Components from './components';
-import * as Pages from './pages';
-
+import * as Components from "./components";
+import * as Pages from "./pages";
 
 const pages = {
-  'login': [ Pages.LoginPage, {test: 'qwerty'} ],
-  'register': [ Pages.RegisterPage ],
-  'chat': [ Pages.ChatPage ],
-  'chatNull': [ Pages.ChatNullPage ],
-  'error500': [ Pages.Error500Page ],
-  'error404': [ Pages.Error404Page ],
-  'profile': [ Pages.ProfilePage ],
-  'profileEdit': [ Pages.ProfileEditPage ],
-  'profileEditPassword': [ Pages.ProfileEditPasswordPage ],
+  login: [Pages.LoginPage, { test: "qwerty" }],
+  register: [Pages.RegisterPage],
+  chat: [Pages.ChatPage],
+  chatNull: [Pages.ChatNullPage],
+  error500: [Pages.Error500Page],
+  error404: [Pages.Error404Page],
+  profile: [Pages.ProfilePage],
+  profileEdit: [Pages.ProfileEditPage],
+  profileEditPassword: [Pages.ProfileEditPasswordPage],
 };
 
-Object.entries(Components).forEach(([ name, component ]) => {
+Object.entries(Components).forEach(([name, component]) => {
   Handlebars.registerPartial(name, component);
 });
 
 function navigate(page: string) {
   //@ts-ignore
-  const [ source, context ] = pages[page];
-  const container = document.getElementById('app')!;
+  const [source, context] = pages[page];
+  const container = document.getElementById("app")!;
   container.innerHTML = Handlebars.compile(source)(context);
 }
 
-document.addEventListener('DOMContentLoaded', () => navigate('login'));
+document.addEventListener("DOMContentLoaded", () => navigate("login"));
 
-document.addEventListener('click', e => {
+document.addEventListener("click", (e) => {
   //@ts-ignore
-  const page = e.target.getAttribute('page');
+  const page = e.target.getAttribute("page");
   if (page) {
     navigate(page);
 
